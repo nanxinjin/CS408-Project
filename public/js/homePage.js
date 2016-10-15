aa12f5698d03feca620f300745355e308c9839da
mtdtao <mtdtao@gmail.com>
2016-10-14 22:13:41 -0400

    updated to latest

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -31,23 +31,34 @@ $form.on('submit', function(event) {
 // password: tao
 
     $.ajax({
-        url: "https://cs408.herokuapp.com/api/login",
+        // url: "https://cs408.herokuapp.com/api/login",
+        url: "http://localhost:3001/api/login",
         data: {
             email: $email.val(),
             password: $password.val()
         },
         type: "POST",
-        dataType : "json"
+        dataType : "json",
+        success: function (data, status, xhr) {
+            // console.log('data: ' + JSON.stringify(data));
+            // console.log('status: ' + status);
+            // console.log('xhr: ' + JSON.stringify(xhr));
+            // console.log('xhr1: ' + xhr.getResponseHeader("Auth"));
+        }
     })// Code to run if the request succeeds (is done);
       // The response is passed to the function
       .done(function( data, textStatus, request ) {
           //TODO: deal with the success situation, store the token to localstorage
-          console.log('1 success login: ' + JSON.stringify(data));
-          console.log('2 success login: ' + request.getResponseHeader('Auth'));
-        //   console.log('3 success login: ' + data.getResponseHeader('Auth'));
-          console.log('4 success login: ' + request.getAllResponseHeaders());
-        //   window.location.href = "userpage.html";
+        //   console.log('1 success login: ' + JSON.stringify(data));
+        //   console.log('2 success login: ' + request.getResponseHeader('auth'));
+        //   console.log('2.1 success login: ' + request.getResponseHeader('Auth'));
+        //   console.log('3 success login: ' + request.getAllResponseHeaders());
+        //   console.log('4 success login: ' + JSON.stringify(request));
+        //   console.log('5 success login: ' + textStatus);
+        //   console.log('6 success login: ' + data.getResponseHeader('Auth'));
+          window.location.href = "userpage.html";
       })
+
       // Code to run if the request fails; the raw request and
       // status codes are passed to the function
       .fail(function( xhr, status, errorThrown ) {
